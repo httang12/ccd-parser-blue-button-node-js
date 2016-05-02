b3f7f3144b20d3031eadae7580b022e4bed73860
Huming Tang <humingtang@tangh02-mbp13r.wireless.nyumc.org>
2016-05-02 12:45:36 -0400

    Added Readme

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is a parser for CCD files using the BlueButton JS module for parsing the CCD Documents to readable formats
--- ccd_parser.js
+++ ccd_parser.js
@@ -1,1 +1,21 @@
-// This script will parse an external CCD xml file and retrieve all data into a object based data structure
+/** 
+*
+* This script will parse an external CCD xml file and retrieve all data into a object based data structure
+*
+*/
+
+// setup global ccd variables
+var xml_path = "/var/www/html/ccd_parser/data_sample/xml/ccd_tang.xml";
+
+// load the file system library
+var fs = require('fs');
+
+// load the bluebutton system
+var BlueButton = require('bluebutton');
+
+// load the ccd file path
+var xml = fs.readFileSync(xml_path, 'utf-8');
+var myRecord = BlueButton(xml);
+
+// Log the demographics data
+console.log(myRecord.data.demographics.json());
